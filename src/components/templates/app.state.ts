940a370cb30ad08977f0f7e8459c82c42eb75687
Miguel Buca <miguelpedrobuca@gmail.com>
2022-10-24 19:43:48 +0100

    feat[group-card]: created

--- src/components/templates/app.state.ts
+++ src/components/templates/app.state.ts
@@ -8,6 +8,20 @@ import user from "@assets/svg/user.svg";
 import heart from "@assets/svg/heart.svg";
 import image from "@assets/svg/image.svg";
 import game from "@assets/svg/game.svg";
+import { GroupItemType } from "@components/organisms/card";
+
+/**
+ * static avatars
+ */
+
+import alexiaDoe from "@assets/svg/avatar/alexia-doe.svg";
+import gabiRosa from "@assets/svg/avatar/gabi-rosa.svg";
+import hillaryJack from "@assets/svg/avatar/hillary-jack.svg";
+import pietroLan from "@assets/svg/avatar/pietro-lan.svg";
+import luaFox from "@assets/svg/avatar/lua-fox.svg";
+import alexHarry from "@assets/svg/avatar/alex-harry.svg";
+import alexiaDoe1 from "@assets/svg/avatar/alexia-doe-1.svg";
+import maxHook from "@assets/svg/avatar/max-hook.svg";
 
 export const useAppTemplateState = () => {
   const menu: MenuType = [
@@ -34,8 +48,44 @@ export const useAppTemplateState = () => {
       imageURL: game,
     },
   ];
+  const friends: GroupItemType[] = [
+    {
+      name: "Alexia Doe",
+      img: alexiaDoe,
+    },
+    {
+      name: "Gabi Rosa",
+      img: gabiRosa,
+    },
+    {
+      name: "Hillary Jack",
+      img: hillaryJack,
+    },
+    {
+      name: "Pietro Lan",
+      img: pietroLan,
+    },
+    {
+      name: "Lua Fox",
+      img: luaFox,
+      useSelo: true,
+    },
+    {
+      name: "Alex Harry",
+      img: alexHarry,
+    },
+    {
+      name: "Alexia Doe",
+      img: alexiaDoe1,
+    },
+    {
+      name: "Max Hook",
+      img: maxHook,
+    },
+  ];
 
   return {
     menu,
+    friends,
   };
 };
--- src/components/templates/app.template.tsx
+++ src/components/templates/app.template.tsx
@@ -2,27 +2,36 @@ import { Aside, Header } from "@components/organisms";
 import React, { FC, HTMLAttributes } from "react";
 import user from "@assets/svg/avatar/user.svg";
 import { useAppTemplateState } from "./app.state";
+import { GroupCard } from "@components/organisms/card";
 
 export const AppTemplate: FC<HTMLAttributes<HTMLDivElement>> = ({
   children,
 }) => {
-
-  const { menu } = useAppTemplateState()
+  const { menu, friends } = useAppTemplateState();
 
   return (
     <div className="fixed flex flex-col w-full h-full container dark:bg-dark-primary-1 dark:text-dark-font">
       <div>
-        <Header/>
+        <Header />
       </div>
       <div className="grid gap-[30px] grid-cols-[270px_1fr_270px] w-full h-full">
         <div>
-          <Aside avatar={user} menu={menu}/>
+          <Aside avatar={user} menu={menu} />
         </div>
+        <div>{children}</div>
         <div>
-          {children}
-        </div>
-        <div>
-          test
+          <div className="grid grid-rows-2 gap-[30px]">
+            <div>
+              <GroupCard
+                title="Amigos"
+                length={51}
+                data={friends}
+              />
+            </div>
+            <div>
+              <GroupCard title="Comunidades" length={313} data={[]} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
