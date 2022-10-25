bf7b3154dbb21dbbd46c1a9f0b97311c67eb8cf3
Miguel Buca <miguelpedrobuca@gmail.com>
2022-10-25 05:22:13 +0100

    fix[app-template]: generic

--- src/components/templates/app.template.tsx
+++ src/components/templates/app.template.tsx
@@ -10,22 +10,32 @@ export const AppTemplate: FC<HTMLAttributes<HTMLDivElement>> = ({
   const { menu, friends, communities } = useAppTemplateState();
 
   return (
-    <div className="fixed flex flex-col w-full h-full container dark:bg-dark-primary-1 dark:text-dark-font">
-      <div>
-        <Header />
-      </div>
-      <div className="grid gap-[30px] grid-cols-[270px_1fr_270px] w-full h-full">
-        <div className="relative overflow-y-auto pb-[150px]">
-          <Aside avatar={user} menu={menu} />
+    <div className="fixed flex flex-col w-full h-full dark:bg-dark-primary-1 dark:text-dark-font">
+      <div className="flex flex-col container">
+        <div className="flex items-center w-full">
+          <div className="w-full">
+            <Header />
+          </div>
         </div>
-        <div>{children}</div>
-        <div className="relative overflow-y-auto pb-[150px]">
-          <div className="grid grid-rows-2 gap-[30px]">
-            <div>
-              <GroupCard title="Amigos" length={51} data={friends} />
+        <div className="flex items-center">
+          <div className="grid gap-[30px] grid-cols-[270px_1fr_270px] w-full">
+            <div className="relative overflow-y-auto pb-[150px] h-[100vh]">
+              <Aside avatar={user} menu={menu} />
             </div>
-            <div>
-              <GroupCard title="Comunidades" length={313} data={communities} />
+            <div>{children}</div>
+            <div className="relative overflow-y-auto pb-[150px] h-[100vh]">
+              <div className="grid grid-rows-2 gap-[30px]">
+                <div>
+                  <GroupCard title="Amigos" length={51} data={friends} />
+                </div>
+                <div>
+                  <GroupCard
+                    title="Comunidades"
+                    length={313}
+                    data={communities}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
